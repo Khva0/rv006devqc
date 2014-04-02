42f6e5b3fcfc603e2202e05f8fd06db8bc9ec7d3
Ivan <kvasela@N5010.(none)>
2014-04-02 13:42:36 +0300

    add cooker.py

---
+++ app/models/cooker.py
@@ -0,0 +1,23 @@
+from wrapper import Wrapper
+
+
+class Cooker(object):
+ 	"""docstring for Cooker"""
+ 	def __init__(self):
+ 		self.wrapper = Wrapper()
+
+
+ 	def add_item_category(self, **item_category):
+ 		return self.wrapper.insert(item_category, "categories")
+
+
+ 	def add_item_menu(self, **item_menu):
+ 		return self.wrapper.insert(item_menu, "dishes")
+
+
+ 	def edit_item_category(self, id_category, **item_category):
+ 		self.wrapper.update(item_category, "categories", "where id={0}".format(id_category))
+
+
+ 	def edit_item_menu(self, id_menu, **item_menu):
+ 		self.wrapper.update(item_menu, "dishes", "where id={0}".format(id_menu))
