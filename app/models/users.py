90481eab488de587c6368c46e6289119137ee6ef
Maxim <zanoga.maxim@gmail.com>
2014-04-02 14:45:46 +0300

    add users class

--- app/models/users.py
+++ app/models/users.py
@@ -1,24 +1,29 @@
 from wrapper import Wrapper
+from conf import Conf
 
 class Users(object):
-
+	
 	def __init__(self):
-		pass
+		self.w = Wrapper(Conf().read())	
 
 	def __del__(self):
 		pass
 
-	def adduser(self):
-		pass
+	def adduser(self,fields):		
+		return self.w.insert(fields, "users")
 
-	def edituser(self):
-		print('add')
+	def edituser(self,fields,condition):
+		return self.w.update(fields, "users",condition)
 
-	def deleteuser(self):
-		pass
+	def deleteuser(self,uid):
+		edituser({"status" : "1"}, "where id={0}".format(uid))
+
+	def get_all_users(self):
+		w = Wrapper(Conf().read())
+		return w.select(["*"], ["users"])
 
 if __name__ == '__main__':
-	#u = Users()
-	#u.edituser()
-	w = Wrapper()
-	print w.Wrapper(Conf().read()).select(["id", "fname", "lname"], ["person"])
+	u = Users()
+	#print u.get_all_users()
+	#u.adduser({"id":"4","f_name" : "T1", "l_name" : "uuuu","login":"asdasdasd","password":"asdasdasd","email":"asdasd@asdad","status":"0","id_role":"1"})
+	#u.edituser({"f_name" : "us"},"where id = 3")
