adf9f708c7afd7bb72d66b25176a4baa32d1b024
Ivan <kvasela@N5010.(none)>
2014-04-03 16:06:28 +0300

    corrected method init, change **item_category on item_category and **item_menu on item_menu

--- app/models/cooker.py
+++ app/models/cooker.py
@@ -1,23 +1,27 @@
 from wrapper import Wrapper
-
+from conf import Conf
 
 class Cooker(object):
  	"""docstring for Cooker"""
  	def __init__(self):
- 		self.wrapper = Wrapper()
+ 		self.wrapper = Wrapper(Conf().read())
 
 
- 	def add_item_category(self, **item_category):
+ 	def add_item_category(self, item_category):
  		return self.wrapper.insert(item_category, "categories")
 
 
- 	def add_item_menu(self, **item_menu):
+ 	def add_item_menu(self, item_menu):
  		return self.wrapper.insert(item_menu, "dishes")
 
 
- 	def edit_item_category(self, id_category, **item_category):
+ 	def edit_item_category(self, id_category, item_category):
  		self.wrapper.update(item_category, "categories", "where id={0}".format(id_category))
 
 
- 	def edit_item_menu(self, id_menu, **item_menu):
+ 	def edit_item_menu(self, id_menu, item_menu):
  		self.wrapper.update(item_menu, "dishes", "where id={0}".format(id_menu))
+
+
+	# cooker = Cooker()
+	# print cooker.add_item_category({"category":"T120"})
