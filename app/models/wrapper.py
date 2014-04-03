3a511280d494828225fc1bca126e84afa45d1be8
Ivan <kvasela@N5010.(none)>
2014-04-03 15:25:36 +0300

    delete argument condition in method insert, rename table_names on table_name in method insert, add return id if true and -1 if false in method insert

--- app/models/wrapper.py
+++ app/models/wrapper.py
@@ -38,26 +38,25 @@ class Wrapper(object):
 			print e
 
 
-	def insert(self, diction, table_names, condition = ""):
-		if not isinstance(table_names, basestring):
-		    table_names = ", ".join(table_names)
-
+	def insert(self, diction, table_name):
 		if isinstance(diction, dict):
 		    keys = ", ".join("{}".format(key) for key in diction)
-		    values = ", ".join("'{}'".format(diction[value]) for value in diction)
+		    values = ", ".join("'{}'".format(diction[key]) for key in diction)
 		    diction = "({0}) values ({1})".format(keys, values)
-		# print "insert into {0}{1} {2}".format(table_names, diction, condition)
+
 		try:
 			cursor = self.connect.cursor()
-			cursor.execute("insert into {0} {1} {2}".format(table_names, diction, condition))
+			cursor.execute("insert into {0} {1}".format(table_name, diction))
 			self.connect.commit()
+			return cursor.lastrowid
 		except Exception, e:
 			print e
+			return -1
 
 			
 	def __del__(self):
 		self.connect.close()
 
 
-#print Wrapper(Conf().read()).select(["id", "fname", "lname"], ["person"])
-# Wrapper(Conf().read()).insert({"fname" : "T1", "lname" : "T2"}, "person")
+# print Wrapper(Conf().read()).select(["id", "category"], "categories")
+# print Wrapper(Conf().read()).insert({"category" : "T7"}, "categories")
