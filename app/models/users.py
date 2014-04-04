2f88eb897016bf1db30496d0e904b9773ff56935
Maxim <zanoga.maxim@gmail.com>
2014-04-04 17:10:20 +0300

    add regexp

--- app/models/users.py
+++ app/models/users.py
@@ -16,6 +16,7 @@ class Users(object):
 				is_email()
 				is_username()
 	"""
+	db_name = "users"
 	def __init__(self):
 		self.w = Wrapper(Conf().read())	
 
@@ -24,27 +25,29 @@ class Users(object):
 
 	def adduser(self,fields):
 		""" Add user method """
-		return self.w.insert(fields, "users")
+		return self.w.insert(fields, self.db_name)
 
 	def edituser(self,fields,condition):
 		""" Edit user method """
-		return self.w.update(fields, "users",condition)
+		return self.w.update(fields, self.db_name,condition)
 
 	def deleteuser(self,uid):
 		""" Delete user method """
-		self.edituser({"status" : "1"}, "where id={0}".format(uid))
+		self.edituser({"status" : "0"}, "where id={0}".format(uid))
 
 	def get_all_users(self):
 		""" Get All user list method """
-		return self.w.select(["*"], ["users"])
+		return self.w.select(["*"], [self.db_name])
 
 	def get_permission(self,uname):
 		""" Get user permission method """
-		return self.w.select("id_role", "users","where login='{0}'".format(uname))
+		return self.w.select("id_role", self.db_name,"where login='{0}'".format(uname))
 
 	def set_permission(self,uname,role):
 		""" Set user permission method """
-		return self.w.update("id_role=2", "users","where login='{1}'".format(role,uname)) # add id_role or name_role ???
+		if role == 'admin': self.w.update("id_role=1", self.db_name,"where login='{0}'".format(uname)) # add id_role or name_role ???
+		if role == 'user': self.w.update("id_role=2", self.db_name,"where login='{0}'".format(uname))
+		if role == 'user': self.w.update("id_role=3", self.db_name,"where login='{0}'".format(uname))
 
 	def validateuser(self,uname,passwd,mail):
 		""" Validate all users fields """
@@ -63,14 +66,14 @@ class Users(object):
 	def is_email(self,email):
 		""" Check mail """
 		result = None
-		try: result = re.match('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email).group(0)
+		try: result = re.match('^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$', email).group(0)
 		except: pass
 		return result is not None
     
 	def is_username(self,uname):
 		""" check username """
 		result = None
-		try: result = re.match('^[a-zA-Z0-9_\.+-@#]+$', uname).group(0)
+		try: result = re.match('^[a-zA-Z0-9_\.+-@#]{3,28}$', uname).group(0)
 		except: pass
 		return result is not None
 
@@ -80,5 +83,5 @@ if __name__ == '__main__':
 	#u.adduser({"id":"4","f_name" : "T1", "l_name" : "uuuu","login":"asdasdasd","password":"asdasdasd","email":"asdasd@asdad","status":"0","id_role":"1"})
 	#u.edituser({"f_name" : "us"},"where id = 3")
 	#print u.get_permission('admin')
-	#print u.set_permission('asdasdasd', 2)
+	print u.set_permission('admin', "admin")
 	#print u.validateuser('admin','3j9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2','test@test.com')
