34c26aacf1af784dc675c9865da7125a538dbc4c
semolex <semolex@semolex-desktop.(none)>
2014-04-05 14:02:17 +0300

    added static routes and simple index page

--- runserver.py
+++ runserver.py
@@ -1,1 +1,21 @@
-from flask import Flask
+from flask import *
+
+app = Flask(__name__) 
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/admin')
+def admin_usr():
+	return render_template('admin.html')
+
+@app.route('/cooker')
+def cooker_usr():
+	return render_template('cooker.html')	
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
