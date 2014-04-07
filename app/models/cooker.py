005259fb13983b8ad546089aafa83237bf2e7100
semolex <semolester@gmail.com>
2014-04-08 01:08:51 +0300

    added method for getting all dishes from DB

--- app/models/cooker.py
+++ app/models/cooker.py
@@ -1,27 +1,30 @@
 from wrapper import Wrapper
-from conf import Conf
+
 
 class Cooker(object):
- 	"""docstring for Cooker"""
- 	def __init__(self):
- 		self.wrapper = Wrapper()
+    """docstring for Cooker"""
 
+    db_name = "dishes"
 
- 	def add_item_category(self, item_category):
- 		return self.wrapper.insert(item_category, "categories")
+    def __init__(self):
+        self.wrapper = Wrapper()
 
+    def get_all_dishes(self):
+        """ Get all dishes from DB """
+        return self.wrapper.select(["*"], [self.db_name])
 
- 	def add_item_menu(self, item_menu):
- 		return self.wrapper.insert(item_menu, "dishes")
+    def add_item_category(self, item_category):
+        return self.wrapper.insert(item_category, "categories")
 
+    def add_item_menu(self, item_menu):
+        return self.wrapper.insert(item_menu, "dishes")
 
- 	def edit_item_category(self, id_category, item_category):
- 		return self.wrapper.update(item_category, "categories", "where id={0}".format(id_category))
+    def edit_item_category(self, id_category, item_category):
+        return self.wrapper.update(item_category, "categories", "where id={0}".format(id_category))
 
-
- 	def edit_item_menu(self, id_menu, item_menu):
- 		return self.wrapper.update(item_menu, "dishes", "where id={0}".format(id_menu))
+    def edit_item_menu(self, id_menu, item_menu):
+        return self.wrapper.update(item_menu, "dishes", "where id={0}".format(id_menu))
 
 
 #cooker = Cooker()
-#print cooker.add_item_category({"category":"T120"})
+#print cooker.get_all_dishes()
