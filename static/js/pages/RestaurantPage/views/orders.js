1ccf55b4fbbce621259f4cc4a67e377e3b6786f1
warrax <dev.rivne@gmail.com>
2014-04-19 19:42:03 +0300

    update

--- static/js/pages/RestaurantPage/views/orders.js
+++ static/js/pages/RestaurantPage/views/orders.js
@@ -1,3 +1,51 @@
+<<<<<<< HEAD
+var Order = Backbone.Model.extend({
+	urlRoot: '/orders',
+});
+
+var Orders = Backbone.Collection.extend({
+	url: '/orders',	
+	model: Order
+});
+
+var orders_view = Backbone.View.extend({
+    el: '#div3',
+    render: function () {
+      var self = this;
+      var orders = new Orders();
+      orders.fetch({
+        success: function (orders) {
+          var template = _.template($('#orders_template').html(), {orders: orders});
+          self.$el.html(template);
+        }
+      })
+    }
+  });
+
+
+var view = new orders_view();
+
+
+var Router = Backbone.Router.extend({
+    routes: {
+      "orders": "orders",
+      "edit_order": "edit_order",
+      "edit_order/:id": "edit_order",
+    }
+});
+
+var router = new Router;
+router.on('route:orders', function() {
+	view.render();
+})
+router.on('route:edit_order', function(id) {
+  
+})
+router.on('route:edit_order', function() {
+	view.render();
+})
+Backbone.history.start();
+=======
 $(document).ready(function() {
 
     var Order = Backbone.Model.extend({
@@ -66,3 +114,4 @@ $(document).ready(function() {
     view_orders.render();
 
 });
+>>>>>>> cc83c9a5f6dc10d76404942b22c7002e969a5aee
