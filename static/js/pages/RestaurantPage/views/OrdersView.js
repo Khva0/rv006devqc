e91b8c51f308bd1ca2e3db2b6980a37fdb1da90a
warrax <dev.rivne@gmail.com>
2014-04-20 01:49:32 +0300

    update

--- static/js/pages/RestaurantPage/views/OrdersView.js
+++ static/js/pages/RestaurantPage/views/OrdersView.js
@@ -1,13 +1,22 @@
-var orders_view = Backbone.View.extend({
-    el: '#div3',
-    render: function () {
-      var self = this;
-      var orders = new Orders();
-      orders.fetch({
-        success: function (orders) {
-          var template = _.template($('#orders_template').html(), {orders: orders});
-          self.$el.html(template);
-        }
-      })
-    }
-  });
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'text!pages/RestaurantPage/templates/orders.html'
+], function($, _, Backbone, OrdersTemplate){
+
+	var orders_view = Backbone.View.extend({
+	    el: '#div3',
+	    render: function () {
+	      var self = this;
+	      var orders = new Orders();
+	      orders.fetch({
+	        success: function (orders) {
+	          var template = _.template(OrdersTemplate, {orders: orders});
+	          self.$el.html(template);
+	        }
+	      })
+	    }
+	  });
+	return orders_view;
+});
