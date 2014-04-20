0669c17534225de2fc789141bfe321688d12997e
warrax <dev.rivne@gmail.com>
2014-04-20 17:08:52 +0300

    update

--- static/js/pages/RestaurantPage/collections/orders_collection.js
+++ static/js/pages/RestaurantPage/collections/orders_collection.js
@@ -1,4 +1,14 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  "pages/RestaurantPage/models/orders_model"
+
+], function($, _, Backbone, Order){
+
 var Orders = Backbone.Collection.extend({
 	url: '/getOrders',	
 	model: Order
+});
+	return Orders;
 });
--- static/js/pages/RestaurantPage/models/orders_model.js
+++ static/js/pages/RestaurantPage/models/orders_model.js
@@ -1,3 +1,12 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+
+], function($, _, Backbone){
+
 var Order = Backbone.Model.extend({
 	urlRoot: '/getOrders',
+});
+	return Order;
 });
