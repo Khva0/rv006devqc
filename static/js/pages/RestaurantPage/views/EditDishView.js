fd9eceb8d5c0d585d8434b8b11f2976721b4809b
Ivan Kvas <kvasela@gmail.com>
2014-04-29 12:52:04 +0300

    Change EditDishView and EditDishTemplate

--- static/js/pages/RestaurantPage/views/EditDishView.js
+++ static/js/pages/RestaurantPage/views/EditDishView.js
@@ -2,16 +2,27 @@ define([
         "underscore",
         "jquery",
         "backbone",        
-        "text!pages/RestaurantPage/templates/EditDishTemplate.html"
-    ], function(_, $, Backbone, EditDishTemplate) {
-            var EditDishView =  Backbone.View.extend({
+        "text!pages/RestaurantPage/templates/EditDishTemplate.html",
+        "pages/RestaurantPage/models/EditDishModel",
+        "pages/RestaurantPage/collections/EditDishCollection"
+    ], 
+    function(_, $, Backbone, EditDishTemplate, EditDishModel, EditDishCollection) {
+            return Backbone.View.extend({
                 el: '#data_table',
-                render: function(){
-                    console.log('render');
-                    var template = _.template(EditDishTemplate);
-                    this.$el.html(template);
+                initialize: function(){
+                    dish = new EditDishCollection();
+                },
+                render: function(id){
+                    self = this;
+                    editDish = dish.set(id); //pass id for fetch
+                    editDish.fetch({
+                        success: function(dish){
+                            template = _.template(EditDishTemplate, dish);
+                            self.$el.html(template);
+                            $('#content').html('');
+                        }
+                    });
                 }
             });
-            return EditDishView;
-        }
+    }
 );
