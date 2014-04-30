abe93c3222bd074dbbb3625e25de927b40c1c7f5
semolex <semolester@gmail.com>
2014-04-30 13:49:44 +0300

    Added icons, new waiter template, new fetch for dishes, small css changes

--- static/js/pages/RestaurantPage/views/WaiterView.js
+++ static/js/pages/RestaurantPage/views/WaiterView.js
@@ -2,19 +2,27 @@ define([
         "underscore",
         "backbone",
         "jquery",
-        "text!pages/RestaurantPage/templates/WaiterTemplate.html"
+        "text!pages/RestaurantPage/templates/WaiterTemplate.html",
+        "pages/RestaurantPage/collections/DishesCollection"
     ],
 
-    function(_, Backbone, $, WaiterTemplate) {
+    function(_, Backbone, $, WaiterTemplate, DishesCollection) {
         return Backbone.View.extend({
-
 
             el: $('#content'),
 
             render: function() {
-
-                this.$el.html(WaiterTemplate);
+                var that = this;
+                var dishes = new DishesCollection();
+                dishes.fetch({
+                    success: function(dishes) {
+                        var template = _.template(WaiterTemplate, {
+                            dishes: dishes
+                        });
+                        that.$el.html(template);
+                    }
+                });
             }
         });
-    }
-);
+        return WaiterView;
+    });
