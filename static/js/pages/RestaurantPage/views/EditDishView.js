2a8825419a56fcbb2483ac22e3dcfb552054ca1c
Ivan Kvas <kvasela@gmail.com>
2014-05-02 18:10:23 +0300

    added click on popup

--- static/js/pages/RestaurantPage/views/EditDishView.js
+++ static/js/pages/RestaurantPage/views/EditDishView.js
@@ -10,28 +10,29 @@ define([
 
     ], 
     function(_, $, Backbone, form2js, EditDishTemplate, EditDishModel, EditDishCollection, StatusesCollection) {
-            return Backbone.View.extend({
-                events:{
-                    'click #save_dish': 'save_dish'
-                },
-                el: '#data_table',
-                initialize: function(){
-                    dish = new EditDishCollection();
-                    statuses = new StatusesCollection();
-                },
-                render: function(id_dish){
-                    self = this;
-                    dish = dish.set(id_dish); //pass id for fetch
-                    $.when(dish.fetch(), statuses.fetch()).done(function(){
-                        template = _.template(EditDishTemplate, dish.toJSON());
-                        self.$el.html(template);
-                        $('#content').html('');
-                    });
-                },
-                save_dish: function(){
-                    var data = form2js('update_menu_form', '.', true);
-                    new EditDishModel().save(data);
-                }
-            });
+        return Backbone.View.extend({
+            events:{
+                'click #save_dish': 'save_dish'
+            },
+            el: '#content',
+            initialize: function(){
+                dish = new EditDishCollection();
+                statuses = new StatusesCollection();
+            },
+            render: function(id_dish){
+                self = this;
+                dish = dish.set(id_dish); //pass id for fetch
+                $.when(dish.fetch(), statuses.fetch()).done(function(){
+                    template = _.template(EditDishTemplate, dish.toJSON());
+                    self.$el.html(template);
+                    $('.popup__overlay').css('display', 'block');
+                });
+            },
+            save_dish: function(){
+                var data = form2js('update_menu_form', '.', true);
+                new EditDishModel().save(data);
+                window.history.back();
+            }
+        });
     }
 );
