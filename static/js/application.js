7b15313905c714cee0b354b22f73394c86642925
warrax <dev.rivne@gmail.com>
2014-05-20 00:57:20 +0300

    update

--- static/js/application.js
+++ static/js/application.js
@@ -1,16 +1,19 @@
 define([
-        "pages/RestaurantPage/routers/routes"
+        "pages/RestaurantPage/routers/routes",
+        "pages/RestaurantPage/views/BucketView"
         //"pages/RestaurantPage/collections/UsersCollection"
         //"pages/RestaurantPage/models/User"
     ],
 
-    function(routes, User) {
+    function(routes, BucketView, User) {
         var _public = {};
         var _private = {};
 
         _private.init = function() {
             // user = new User();
             router = new routes();
+            cartView = new BucketView();
+            cartView.render();
         };
 
         _public.start = function() {
